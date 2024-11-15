44366f2afe8e64e9a0e3817b2593f74d293bf303
Alexander Luzarraga <alexanderluzarraga.alu@iespacomolla.es>
2024-11-15 12:42:02 +0100

    Calculadora simple

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+num1 = float(input("Introduce un número: "))
+num2 = float(input("Introduce un segundo número: "))
+
+print("La suma de los numeros", num1+num2)
+print("La resta de los numeros", num1-num2)
+print("La multiplicación de los numeros", num1*num2)
+print("La división entera de los numeros", num1//num2)
+print("La división de los numeros", num1/num2)
